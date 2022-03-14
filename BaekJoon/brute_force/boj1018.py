57a820808523ead8e0fa6de7a2a9546d73c3f3f7
= <sskong777@gmail.com>
2022-03-15 00:17:42 +0900

    0315 algo

---
+++ BaekJoon/brute_force/boj1018.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+# def check_board(arr, M, N):
+#     B_arr = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+#     W_arr = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+#     for i in range(M):
+
+N, M = map(int,input().split())
+# arr = [input() for _ in range(N)]
+arr = []
+for i in range(N):
+    arr.append(input())
+# pprint(arr)
+
+count = []
+for i in range(N-7):
+    for j in range(M-7):
+        w_count = 0     # w가 먼저 시작하는 경우
+        b_count = 0     # b가 먼저 시작하는 경우
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if (k+l) % 2 == 0:
+                    if arr[k][l] != 'W':
+                        w_count += 1
+                    if arr[k][l] != 'B':
+                        b_count += 1
+                else:
+                    if arr[k][l] != 'B':
+                        w_count += 1
+                    if arr[k][l] != 'W':
+                        b_count += 1
+        count.append(min(w_count,b_count))
+print(min(count))
+
---
+++ BaekJoon/brute_force/boj7568.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = []
+for n in range(N):
+    x, y = map(int,input().split())
+    arr.append((x,y))
+# print(arr)
+
+rank = [1] * N
+for i in range(N):
+    for j in range(N):
+        if j == i:
+            continue
+        else:
+          # print(arr[j][0])
+            if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
+                rank[i] += 1
+print(' '.join(map(str,rank)))
