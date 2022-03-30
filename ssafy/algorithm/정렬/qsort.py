32dbfd042269d8ead53bd8dd305a71780f1db3db
= <sskong777@gmail.com>
2022-03-31 01:33:41 +0900

    0331 algo3

--- ssafy/algorithm/정렬/qsort.py
+++ ssafy/algorithm/정렬/qsort.py
@@ -9,14 +9,19 @@ def partition (a, begin, end) :
     L = begin
     R = end
     while L < R :
-        while(L<R and a[L]< a[pivot]) : L += 1
-        while(L<R and a[R]>=a[pivot]) : R -= 1
+        while(L<R and a[L]< a[pivot]) :
+            L += 1
+        while(L<R and a[R]>=a[pivot]) :
+            R -= 1
         if L < R :
-            if L==pivot : pivot = R
+            if L==pivot :
+                pivot = R
             a[L], a[R] = a[R], a[L]
     a[pivot], a[R] = a[R], a[pivot]
     return R
 
-a = [69, 10, 30, 2, 16, 8, 31, 22]
-quickSort(a, 0, 7)
+# a = [69, 10, 30, 2, 16, 8, 31, 22]
+a= [11,45,23,81,28,34]
+end = len(a)
+quickSort(a, 0, end-1)
 print(a)
