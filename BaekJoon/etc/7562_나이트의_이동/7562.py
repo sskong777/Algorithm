1a9e3ce7978cf714693912797034f2218a3ea30f
sskong777 <sskong777@gmail.com>
2022-04-27 00:49:29 +0900

    0427 algo - dp

--- BaekJoon/etc/7562_나이트의_이동/7562.py
+++ BaekJoon/etc/7562_나이트의_이동/7562.py
@@ -6,6 +6,39 @@ sys.stdin = open('input.txt', 'r')
 def bfs(si,sj):
     q = deque()
     q.append((si,sj))
+    v[si][sj] = 1
+
+    while q:
+        ci,cj = q.popleft()
+        if ci==ei and cj==ej:
+            return v[ci][cj]
+
+        for di,dj in ((1,2),(-1,2),(-1,-2),(1,-2),(2,1),(2,-1),(-2,1),(-2,-1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj]==0:
+                q.append((ni,nj))
+                v[ni][nj] = v[ci][cj] + 1
+import sys
+from collections import deque
+
+sys.stdin = open('input.txt', 'r')
+
+def bfs(si,sj):
+    q = deque()
+    q.append((si,sj))
+    v[si][sj] = 1
+
+    while q:
+        ci,cj = q.popleft()
+        if ci==ei and cj==ej:
+            return v[ci][cj]
+
+        for di,dj in ((1,2),(-1,2),(-1,-2),(1,-2),(2,1),(2,-1),(-2,1),(-2,-1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj]==0:
+                q.append((ni,nj))
+                v[ni][nj] = v[ci][cj] + 1
+
 
 T = int(input())
 for tc in range(T):
@@ -14,3 +47,15 @@ for tc in range(T):
     ei,ej = map(int,input().split())
 
     v = [[0]*N for _ in range(N)]
+    res = bfs(si,sj)
+    print(res-1)
+
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    si,sj = map(int,input().split())
+    ei,ej = map(int,input().split())
+
+    v = [[0]*N for _ in range(N)]
+    res = bfs(si,sj)
+    print(res-1)
---
+++ 이것이코딩테스트다/DynamicProgramming/8_3_fibonaccci_memo.py
@@ -0,0 +1,16 @@
+d = [0]*100
+
+def fibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    # 종료 조건
+    if x==1 or x==2:
+        return 1
+    # 이미 구한 값이라면(메모제이션에 저장되어있는 값이라면)
+    if d[x] != 0:
+        return d[x]
+    # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
+    d[x] = fibo(x-2) + fibo(x-1)
+    return d[x]
+
+ans = fibo(6)
+print(ans)
