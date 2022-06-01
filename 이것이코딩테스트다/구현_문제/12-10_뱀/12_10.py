e758d3461e623918cf998635c05df412e91354d1
sskong777 <sskong777@gmail.com>
2022-06-01 18:02:42 +0900

    algo

--- 이것이코딩테스트다/구현_문제/12-10_뱀/12_10.py
+++ 이것이코딩테스트다/구현_문제/12-10_뱀/12_10.py
@@ -1,18 +1,39 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+from collections import deque
 
 def bam_turn(x,c):
     if c == 'L':
-        x =
+        x = (x-1) % 4   # 상 좌 하 우
+    else:
+        x = (x+1) % 4 # 상 우 하 좌
+    return x
 
 
 
 def start():
+    direction = 1
+    time = 1
+    y,x = 0,0
+    visited = deque([[y,x]])
+    arr[y][x] = 2
+    while True:
+        y,x = y+ dy[direction], x + dx[direction]
+        if 0<= y < N and 0<= x < N and arr[y][x] != 2:
+            if not arr[y][x] == 1:
+                temp_y, temp_x = visited.popleft()
+                arr[temp_y][temp_x] = 0     # 꼬리제거
+            arr[y][x] = 2
+            visited.append([y,x])
+            if time in turn.keys():
+                direction = bam_turn(direction, turn[time])
+            time += 1
+        else:
+            return time
 
 
-
-
+# 상 우 하 좌
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
@@ -23,7 +44,7 @@ arr = [[0] * N for i in range(N)]
 
 for k in range(K):
     r,c = map(int,input().split())
-    arr[r-1][c-1] = 1
+    arr[r-1][c-1] = 1       # 과일 위치에 1 남기기
 
 
 L = int(input())
@@ -31,4 +52,5 @@ turn = {}
 for l in range(L):
     x,c = input().split()
     turn[int(x)] = c
-# print(turn)
+
+print(start())
