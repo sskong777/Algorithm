d6f3baa52fe74443f2f9ea89ae61fd6ee8bdc417
sskong777 <sskong777@gmail.com>
2022-06-06 11:06:40 +0900

    0606

--- 이것이코딩테스트다/구현_문제/12-13_치킨배달/12_13.py
+++ 이것이코딩테스트다/구현_문제/12-13_치킨배달/12_13.py
@@ -1,6 +1,5 @@
 import sys
 sys.stdin = open('input.txt', 'r')
-
 from itertools import combinations
 
 N,M = map(int,input().split())
@@ -19,14 +18,17 @@ for i in range(N):
 comb = list(combinations(chick,M))
 # print(comb)
 
-result = 999999
+result = 1e9
 # 치킨거리의 최솟값 구하기
+# M개의 치킨집들의 조합
 for com in comb:
     temp = 0
     for hi,hj in house:
-        distance = 10000
+        distance = 1e9
         for si,sj in com:
+            # "치킨거리" = 집과 가까운 치킨집 사이의 거리
             distance = min(distance,abs(hi-si)+ abs(hj-sj))
+        # "도시의 치킨거리" = 모든 치킨거리의 합
         temp += distance
     result = min(result,temp)
 
