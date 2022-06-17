8ddd6ca41b31639c6a24f24edb7978a3d62ef739
sskong777 <sskong777@gmail.com>
2022-06-17 23:37:25 +0900

    0617 algo

---
+++ 이것이코딩테스트다/그래프이론_문제/18-44_행성터널/행성터널.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+N = int(input())
+
