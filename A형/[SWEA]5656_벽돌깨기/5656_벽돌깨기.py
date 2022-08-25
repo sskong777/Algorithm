6f8f43ee79d0db3c8ef6ac016647cc3d8437f354
sskong777 <sskong777@gmail.com>
2022-08-25 17:52:43 +0900

    0826 algo

--- A형/[SWEA]5656_벽돌깨기/5656_벽돌깨기.py
+++ A형/[SWEA]5656_벽돌깨기/5656_벽돌깨기.py
@@ -1,2 +1,10 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+
+T = int(input())
+
+for tc in range(T):
+    N, W, H = map(int,input().split())
+    arr = [list(map(int,input().split())) for _ in range(H)]
+    # print(arr)
+    
