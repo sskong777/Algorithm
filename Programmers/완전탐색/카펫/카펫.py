1fc5f7f2ada136134fe1409f795ef368092f8ba6
sskong777 <sskong777@gmail.com>
2022-09-04 20:59:50 +0900

    0904 algo

---
+++ Programmers/완전탐색/카펫/카펫.py
@@ -0,0 +1,14 @@
+# 조건1. x >= y
+# 조건2.(x-2) * (y-2) = yellow
+# 조건3. 2*x + 2*y -4 = brown
+
+def solution(brown, yellow):
+    rec_size = brown + yellow
+    for y in range(1,rec_size+1):
+        if (rec_size / y) % 1 == 0:          # 나누어 떨어질 때
+            x = rec_size / y
+            if x >= y:
+                if 2*x + 2*y == brown + 4:
+                    return [int(x),int(y)]
+
+print(solution(24,24))
