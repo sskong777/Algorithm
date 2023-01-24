06d928cc1ba3fd399f3731f6fd54f1d269e46640
= <sskong777@gmail.com>
2023-01-24 19:50:06 +0900

    0124 algo

--- 이것이코딩테스트다/DFS&BFS_문제/14502_연구소/14052_re.py
+++ 이것이코딩테스트다/DFS&BFS_문제/14502_연구소/14052_re.py
@@ -1,2 +1,12 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+
+N,M = map(int,input().split())
+
+# 지도 입력받기
+arr = [(list(map(int,input().split()))) for _ in range(N)]
+virus = []
+empty = []
+walls = 0
+safe = 0
+
