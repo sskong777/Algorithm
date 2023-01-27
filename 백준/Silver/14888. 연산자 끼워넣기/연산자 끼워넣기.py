a3a05b1884fdc36bae0b1c31a777f37868520a2a
sskong777 <97655673+sskong777@users.noreply.github.com>
2023-01-27 18:13:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 204 ms, Memory: 118648 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,46 +1,28 @@
-from itertools import permutations
-
-mmax = -1000000000
-mmin = 1000000000
-
-
 N = int(input())
-arr = list(map(int,input().split()))
-operator = list(map(int,input().split()))
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))  # +, -, *, //
 
-oper_list = []
-for i in range(len(operator)):
-    if i == 0:
-        for j in range(operator[i]):
-            oper_list.append('+')
-    elif i == 1:
-        for j in range(operator[i]):
-            oper_list.append('-')
-    elif i == 2:
-        for j in range(operator[i]):
-            oper_list.append('*')
-    elif i == 3:
-        for j in range(operator[i]):
-            oper_list.append('%')
+maximum = -1e9
+minimum = 1e9
 
-oper = permutations(oper_list)
-for i in oper:
-    ssum = arr[0]
 
-    for j in range(1,N):
-        if i[j-1] == '+':
-            ssum += arr[j]
-        elif i[j-1] =='-':
-            ssum -= arr[j]
-        elif i[j-1] =='*':
-            ssum *= arr[j]
-        elif i[j-1] =='%':
-            ssum = int(ssum / arr[j])
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
 
-    if ssum > mmax:
-        mmax = ssum
-    if ssum < mmin:
-        mmin = ssum
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
 
-print(mmax)
-print(mmin)
+
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
