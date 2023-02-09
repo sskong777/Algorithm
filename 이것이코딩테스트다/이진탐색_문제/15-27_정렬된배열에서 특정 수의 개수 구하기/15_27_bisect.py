cb659656bea974faf2e0d37c98648d021e72a938
sskong777 <sskong777@gmail.com>
2023-02-09 13:56:53 +0900

    0209 algo

--- 이것이코딩테스트다/이진탐색_문제/15-27_정렬된배열에서 특정 수의 개수 구하기/15_27_bisect.py
+++ 이것이코딩테스트다/이진탐색_문제/15-27_정렬된배열에서 특정 수의 개수 구하기/15_27_bisect.py
@@ -1,10 +1,20 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+from bisect import bisect_left, bisect_right
 
-def count_by_range(arr)
+def count_by_range(arr,left_value,right_value):
+    left_index = bisect_left(arr,left_value)
+    right_index = bisect_right(arr,right_value)
+    return right_index - left_index
 
 
 N,x = map(int,input().split())
 arr = list(map(int,input().split()))
 
+count = count_by_range(arr,x,x)
+
+if count == 0:
+    print(-1)
+else:
+    print(count)
