90198ef04b2bae0e3d5d41c2eae5599d3f6fc5b9
= <sskong777@gmail.com>
2023-02-17 20:04:02 +0900

    0217 algo

---
+++ 프로그래머스/lv1/42576_완주하지못한선수.py
@@ -0,0 +1,21 @@
+# 해쉬로 풀이
+
+def solution(participant, completion):
+
+    hash_dict = {}
+    sumHash = 0
+
+    # 1. Hash : Participant의 dictionary 만들기
+    # 2. Participant의 sum(hash) 구하기
+    for part in participant:
+        hash_dict[hash(part)] = part
+        sumHash += hash(part)
+
+    # 3. completion의 sum(hash) 빼기
+    for comp in completion:
+        sumHash -= hash(comp)
+
+    # 4. 남은 값이 완주하지 못한 선수의 hash 값이 된다.
+
+    return hash_dict[sumHash]
+
