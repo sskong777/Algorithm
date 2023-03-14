44c50ff59d794786bf5b6fbc38326ed986a9ab68
sskong777 <sskong777@gmail.com>
2023-03-14 09:03:17 +0900

    0314 algo

---
+++ 프로그래머스/lv2/12941_최솟값만들기.py
@@ -0,0 +1,19 @@
+def solution(A, B):
+    answer = 0
+
+    ans1 = 0
+    Asort1 = sorted(A)
+    Bsort1 = sorted(B, reverse=True)
+
+    ans2 = 0
+    Asort2 = sorted(A, reverse=True)
+    Bsort2 = sorted(B)
+
+    llen = len(A)
+
+    for i in range(llen):
+        ans1 += Asort1[i] * Bsort1[i]
+        ans2 += Asort2[i] * Bsort2[i]
+
+    answer = min(ans1, ans2)
+    return answer
