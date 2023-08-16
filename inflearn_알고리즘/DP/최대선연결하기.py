d04e4f450e8738cd98afdafe675a93297447e3e9
sskong777 <sskong777@gmail.com>
2023-08-16 20:49:24 +0900

    LIS

---
+++ inflearn_알고리즘/DP/최대선연결하기.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = [0] + list(map(int,input().split()))
+
+dp = [0] * (n+1)
+dp[1] = 1
+res = 0
+for i in range(2,n+1):
+    max = 0
+    for j in range(i-1,0,-1):
+        if arr[i] > arr[j] and dp[j] > max:
+            max = dp[j]
+
+    dp[i] = max + 1
+    if dp[i] > res:
+        res = dp[i]
+print(res)
