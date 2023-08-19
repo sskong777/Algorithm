2158abc1152b29559d3fbf28a8999efa9cf7a0b7
sskong777 <97655673+sskong777@users.noreply.github.com>
2023-08-19 21:19:30 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 200 ms, Memory: 126848 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,21 +1,17 @@
-import math
+def isPrime(m,n):
+    sieve = [True] * (n+1)
+    root = int(n ** 0.5)
+    sieve[1] = False
+    for i in range(2,root+1):
+        if sieve[i] == True:    # 소수 라면
+            for j in range(i+i,n+1,i):
+                sieve[j] = False
+    return [i for i in range(m,n+1) if sieve[i] == True]
 
-def isPrime(x):
-    if x == 1:
-        return False
-    for i in range(2, int(math.sqrt(x)+1)):
-        if x % i == 0:
-            return False
-    return True
-
-def isPalindrome(x):
-    if str(x) == str(x)[::-1]:
-        return True
-    return False
-
-N = int(input())
-while True:
-    if isPrime(N) and isPalindrome(N):
-        print(N)
+num = int(input())
+prime_list = isPrime(num,1005000)
+# print(prime_list)
+for prime in prime_list:
+    if str(prime) == str(prime)[::-1]:
+        print(prime)
         break
-    N += 1
