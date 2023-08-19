ece4fb0113787fcdbc99ab59ef8abcc7cacede30
sskong777 <sskong777@gmail.com>
2023-08-19 19:46:20 +0900

    greedy final

---
+++ Do_it_알고리즘/그리디/회의실배정하기.py
@@ -0,0 +1,35 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    start,end = map(int,input().split())
+    arr.append((start,end))
+arr.sort(key=lambda x:(x[1],x[0]))
+
+print(arr)
+answer = 0
+
+end_time = -1
+for i in range(n):
+    if arr[i][0] >= end_time:
+        end_time = arr[i][1]
+        answer += 1
+
+print(answer)
+
+
+# n = int(input())
+# arr = [[0] * 2 for _ in range(n)]
+#
+# for i in range(n):
+#     s,e = map(int,input().split())
+#     arr[i][0] = e
+#     arr[i][1] = s
+# arr.sort()
+# count = 0
+# end = -1
+#
+# for i in range(n):
+#     if arr[i][1] >= end:
+#         end = arr[i][0]
+#         count += 1
+# print(count)
